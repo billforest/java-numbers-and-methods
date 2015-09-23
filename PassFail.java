97561642e071d1ab47006354b3683dbc35ae741e
jaehyung Lim <jaehyunglimsp15@gpa.ac.kr>
2015-09-23 13:55:44 +0900

    Wrote 4 classes

---
+++ PassFail.java
@@ -0,0 +1,39 @@
+/*
+ * Project: PassFail.java
+ * Description: Returns true/false if you passed/failed a class/test
+ * Name: Jaehyung Lim
+ * Date: Sept 23, 2015
+ */
+
+import java.util.Scanner;
+
+public class PassFail {
+
+  public static void main( String[] args ) {
+  
+    Scanner s = new Scanner( System.in );
+    int score; // usecore
+    
+    System.out.println( "Enter your PSAT score: " ); // prompt
+    score = s.nextInt();
+    
+    System.out.println( " Passed : " + checkPassed( score ) ); // method
+    
+    
+  } // end main method
+  
+  public static boolean checkPassed( int psatScore ) {
+    
+    if ( psatScore > 0 && psatScore <= 1520 ) {
+      if ( psatScore < 320 ) {
+        return false;
+      } else {
+        return true;
+      }
+      } else {
+        return false;
+      }
+        
+  } // end checkPassed method
+  
+} // end class
---
+++ TableOfBases.java
@@ -0,0 +1,27 @@
+/*
+ * Project: TableOfBases.java
+ * Description: Prints a table of Decimal, Binary, Octal, Hex, ASCII characters
+ * Name: Jaehyung Lim
+ * Date: Sept 23, 2015
+ */
+
+public class TableOfBases {
+  
+  public static void main( String[] args ) {
+    
+    System.out.println( "TABLE OF BASES" );
+    System.out.println( "Deciaml\tBinary\tOctal\tHex\tASCII" );
+    
+    for ( int i = 65; i <= 90; i++ ) {
+      System.out.print( i );
+      System.out.print( "\t" + Integer.toBinaryString( i ) );
+      System.out.print( "\t" + Integer.toOctalString( i ) );
+      System.out.print( "\t" + Integer.toHexString( i ) );
+      System.out.print( "\t" + (char)i );
+      System.out.println();
+      
+    }
+    
+  } // end main method
+  
+} // end class
